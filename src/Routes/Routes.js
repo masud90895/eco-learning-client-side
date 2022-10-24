3ab02314159ff6f2738156c72529ff657b35164f
Md Mahafujur Rahaman Masud <masudhossainmbs129@gmail.com>
2022-10-25 02:09:09 +0600

    course component added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,39 +2,45 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../Component/Athentication/Login";
 import SingUp from "../Component/Athentication/SingUp";
 import Courses from "../Component/Courses/Courses";
+import CoursesSiteContain from "../Component/Courses/CoursesSiteContain";
 import Home from "../Component/Home/Home";
+import CoursesOutLet from "../Component/Courses/CoursesOutLet";
 import ErrorPage from "../Component/Main/ErrorPage";
 import Main from "../Component/Main/Main";
 
-export const router = createBrowserRouter(
-    [
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
       {
         path: "/",
-        element: <Main/>,
-        errorElement: <ErrorPage/>,
-        children:[
-          {
-            path: '/',
-            element:<Home/>
-          },
-          {
-            path: 'home',
-            element:<Home/>
-          },
-          {
-            path: 'login',
-            element:<Login/>
-          },
-          {
-            path: 'register',
-            element:<SingUp/>
-          },
-          {
-            path: 'courses',
-            element:<Courses/>,
-            loader: ()=> fetch ("http://localhost:5000")
-          },
-        ]
+        element: <Home />,
       },
-    ]
-    );
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <SingUp />,
+      },
+      {
+        path: "courses",
+        element: <CoursesOutLet />,
+        loader: () => fetch("http://localhost:5000"),
+        children: [
+          {
+            path: "courses",
+            element: <CoursesSiteContain/>
+          },
+        ],
+      },
+    ],
+  },
+]);
