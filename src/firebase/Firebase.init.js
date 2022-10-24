74132501e628c6a3c450fb3a5277750ee9dd7f1e
Md Mahafujur Rahaman Masud <masudhossainmbs129@gmail.com>
2022-10-25 03:43:26 +0600

    envairoment varrieble added

--- src/firebase/Firebase.init.js
+++ src/firebase/Firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCmkpXn2PW6GfSyAgALtw7k_sOIqsFhtFQ",
-  authDomain: "eco-learning-9111b.firebaseapp.com",
-  projectId: "eco-learning-9111b",
-  storageBucket: "eco-learning-9111b.appspot.com",
-  messagingSenderId: "932550546488",
-  appId: "1:932550546488:web:855b62bb56eb0afc3e02cd"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
